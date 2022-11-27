339601bce5a839fc359b98b7128232514094c156
Xir0d <93400818+Xir0d@users.noreply.github.com>
2022-11-27 10:50:15 +0100

    update main.py
    Développement en cours

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-test
+import string
+
+#Importation de la clé privée de codage
+private_key = open('clé privé.key')
+private_key = private_key.read()
+
+  
+#Récupération du décalage des lettres de la clé privée, exemple: a=1, b=2, etc...
+
+NUM = 31
+
+def positions(str):
+    for i in str:
+        print((ord(i) & NUM), end =" ")
+  
+str = private_key
+positions(str)
+
