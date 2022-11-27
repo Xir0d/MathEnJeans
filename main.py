dffe2a12ad4aa4165bd277d115f7e16a38c2fff3
Xir0d <93400818+Xir0d@users.noreply.github.com>
2022-11-27 22:38:37 +0100

    update main.py
    trying to resolve a major problem

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import replace
 #Importation de la clé privée de codage
 private_key = open('clé privé.key')
 private_key = private_key.read()
-print(private_key)
 
 #Définition de la longueur de la clé privée pour calculer ensuite combien avons-nous besoin de la copier
 
@@ -18,25 +17,10 @@ key_count = int(count_letters(key_length))
 #Demande du message à chiffrer avec la clé privée
 message_a_chiffrer = input("Écrivez le message à chiffrer: ")
 
-#Récupération du décalage des lettres de la clé privée, exemple: a=1, b=2, etc...
-
-NUM = 31
-
-def positions(str):
-    for i in str:
-          
-        # Performing AND operation
-        # with number 31
-        print((ord(i) & NUM), end ="")
-  
-str = private_key
-key_number = positions(str)
-fichier = open("test.txt", "r")
-print(fichier)
-
 #Récupération du nombre de lettres à chiffrer dans le message pour définir le nombre de fois que nous avons besoin de la clé de chiffrement privée
 # (+) Division du nombre de caractère par la longueur de la clé de chiffrement privée
 #On ajoute 1 pour éviter 9/2 = 1 fois la clé au lieu de 2
+
 
 def count_letters(msg):
     return len(msg) - msg.count(' ')
@@ -44,10 +28,32 @@ def count_letters(msg):
 msg = message_a_chiffrer
 key_repeat = int(count_letters(msg) / key_count) + 1
 
-#Définition de la liste de décalage à éxécuter (pour chaque lettre) en fonction de la clé privée, ex: 123451234512345
 
-#key_chain = positions(str) * 3
-#print(key_chain)
+#Définition de la liste de décalage à éxécuter (pour chaque lettre) en fonction de la clé privée, ex: abcabcabcabcabcabc
 
-#Création d'une liste avec le décalage à effectuer pour la lettre 1, 2, 3, etc... (selon la longueur nécessaire définis précedemment)
+key_chain = private_key * key_repeat
+
+#Convertion de la liste de décalage (en lettres) vers une liste de décalage en chiffres ex: 123123123123123
+
+NUM = 31
+  
+def positions(str):
+    for i in str:
+        print((ord(i) & NUM), end ="")
+        test = 1
+        return test
+
+  
+str = key_chain
+# position str n'est rien il renvoie a none
+
+#Envoi de la liste de décalage en chiffre dans un fichier txt
+
+print(test)
+
+#SOLUTION: Au lieu d'utiliser une fonction, directement remplacer les a par 1 et b par 2, etc...
+#OBJECTIF: FAIRE SORTIR LA VARIABLE LIGNE 42 DE LA FONCTION POUR POUVOIR L'UTILISER en utilisant return
+
+#PLAN B: utiliser le code d'aikoo
+#PLAN C: remplacer la clé directement en chiffre/avoir une clé prédéfinis
 
--- test.py
+++
@@ -1,5 +0,0 @@
-def count_letters(word):
-    return len(word) - word.count(' ')
-
-word = 'test'
-print(count_letters(word))
